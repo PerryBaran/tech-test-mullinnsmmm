f3ae5f157d302cd5e3de94af01760eedc8c38c34
Matt Mullins <matt.mullins@live.com>
2023-06-22 21:17:21 +0100

    Add Search.js

--- src/conponents/App.js
+++ src/conponents/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles/app.css";
+import Search from "./Search";
 
 const App = () => {
   return (
   <div className="app">
     <p>Hello World!</p>
+    <Search />
     </div>
   );
 }
---
+++ src/conponents/Search.js
@@ -0,0 +1,9 @@
+import { React, useState} from 'react';
+
+function Search () => {
+    return (
+        <>
+        <input type={text}></input>
+        </>
+    )
+}
