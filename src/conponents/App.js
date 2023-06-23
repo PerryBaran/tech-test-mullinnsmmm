3615db7a8221e3149bec31b5e37790d89ad1f3c8
Matt Mullins <matt.mullins@live.com>
2023-06-23 17:36:46 +0100

    Search results returns image

--- src/conponents/App.js
+++ src/conponents/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/app.css";
 import Search from "./Search";
+import SearchResults from './SearchResults';
+
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
   <div className="app">
     <img 
@@ -10,7 +14,8 @@ const App = () => {
     alt="nasaLogo"
     src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
     />
-    <Search />
+    <Search setSearchResults={setSearchResults}/>
+    <SearchResults/>
     </div>
   );
 }
---
+++ src/conponents/SearchResults.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import React from "react";
+
+const SearchResults = () => {
+    return (
+        <div className="searchResults">
+            <img
+            className="search-images"
+            alt="searchResults"
+            src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"/>
+    <>
+    <p>Search Result</p>
+    </>
+    </div>
+    )
+}
+
+export default SearchResults;
