e7de7b03e9c2747a2b7f9638955945a40e2ab991
Matt Mullins <matt.mullins@live.com>
2023-06-24 17:19:49 +0100

    updated tests

--- src/__tests__/SearchResults.test.js
+++ src/__tests__/SearchResults.test.js
@@ -2,10 +2,10 @@ import React from "react";
 import { render } from "@testing-library/react";
 import SearchResults from "../conponents/SearchResults";
 
-describe("Search Results returns image", () => {
-  const { asFragment } = render(<SearchResults />);
-
-  it("renders image", () => {
-    expect(asFragment()).toMatchSnapshot();
-  });
+describe("Search Results", () => {
+    it("renders correctly", () => {
+        const results = [];
+        const { asFragment } = render(<SearchResults results={results}/>);
+        expect(asFragment()).toMatchSnapshot();
+    })
 });
--- src/conponents/SearchResults.js
+++ src/conponents/SearchResults.js
@@ -2,17 +2,21 @@ import React from "react";
 import "../styles/searchresults.css";
 
 const SearchResults = ({ results }) => {
-    if (!results.length) {
-        return <p>No results</p>;
+    if(!results) {
+        return <p></p>
+    } else if (!results.length) {
+        return <p className="results-message">No results</p>
     } else {
     return (
         <>
         <div className="search-results">
             {results.map((result) => (
-                <img className="card-image" src={ result } alt="spaceImage" />
+                <img 
+                key={result}
+                className="card-image" 
+                src={ result } 
+                alt="spaceImage" />
             ))}
-    
-            <p>Search Result</p>
             </div>
         </>
         );
